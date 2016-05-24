8e1760e301b48207a8b7a100cd612c2c0e41ed2c
Fadi Bitar <fadibitar@mail.usf.edu>
2016-05-24 09:06:39 -0400

    version 1.0

---
+++ @162.243.59.186/app.js
@@ -0,0 +1,79 @@
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var bootstrap = require("express-bootstrap-service");
+var router = express.Router();
+
+var contact = require('./routes/contact');
+var education = require('./routes/education');
+var programming = require('./routes/programming');
+var philosophy = require('./routes/philosophy');
+var routes = require('./routes/index');
+var users = require('./routes/users');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+/*
+app.get('/', function (req, res) {
+  console.log("GET '/' ");
+  res.render('../views/index');
+  res.render('../routes/index.js');
+});
+*/
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(bootstrap.serve);
+
+app.use('/', routes);
+app.use('/contact', contact);
+app.use('/education', education);
+app.use('/programming', programming);
+app.use('/philosophy', philosophy);
+app.use('/users', users);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err
+    });
+  });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: {}
+  });
+});
+
+
+module.exports = app;
